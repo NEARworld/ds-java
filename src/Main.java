8664827444d62aa5d8c71bce7a0593bdedcdadab
nearworld <artemismars2@outlook.com>
2024-01-05 01:00:22 +0900

    use add and traverse methods

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,28 @@
+import array_list.IntegerArrayList;
 import linked_list.Node;
 import linked_list.SinglyLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        SinglyLinkedList list = new SinglyLinkedList();
-        list.insert(10, 0);
-        list.insert(20, 1);
-        list.insert(30, 2);
-        list.insert(40, 3);
+//        SinglyLinkedList list = new SinglyLinkedList();
+//        list.insert(10, 0);
+//        list.insert(20, 1);
+//        list.insert(30, 2);
+//        list.insert(40, 3);
+//
+//        list.insert(100, 1);
+//
+//        Node curr = list.head;
+//        for (int i = 0; i < 4; ++i) {
+//            System.out.println(curr.data);
+//            curr = curr.next;
+//        }
 
-        list.insert(100, 1);
+        IntegerArrayList list = new IntegerArrayList();
+        list.add(0, 3);
+        list.add(0, 1);
+        list.add(1, 4);
 
-        Node curr = list.head;
-        for (int i = 0; i < 4; ++i) {
-            System.out.println(curr.data);
-            curr = curr.next;
-        }
-
+        list.traverse();
     }
 }
